0d64f3207187125f47b40ac8dd084fb47bdfca6f
ostanioni <ostanioni@gmail.com>
2019-04-10 11:53:32 +0700

    v.0.0.1

---
+++ .babelrc.js
@@ -0,0 +1,29 @@
+const RUNTIME =  [
+  "@babel/plugin-transform-runtime",
+  {
+    "corejs": false,
+    "helpers": true,
+    "regenerator": true,
+    "useESModules": false
+  }
+]
+const DECORATOR = ["@babel/plugin-proposal-decorators", { "legacy": true }]
+const PROPOSAL = ["@babel/plugin-proposal-class-properties", { "loose" : true }]
+const TRANSFORM_LITERALS = [ "@babel/plugin-transform-template-literals", { "loose": true, } ]
+
+const STYLED = [ 
+  "babel-plugin-styled-components", 
+  { 
+    "fileName": true,
+    "displayName": true,
+    "ssr": false,
+    "minify": true,
+    "pure": true,
+    "transpileTemplateLiterals": true
+  } 
+]
+module.exports = {
+  "presets": [ "@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript", ],
+  "plugins": [ DECORATOR, PROPOSAL, RUNTIME, PRISM, TRANSFORM_LITERALS, STYLED ],
+}
+ 
---
+++ postcss.config.js
@@ -0,0 +1,23 @@
+/* eslint-disable */
+/*tslint:disabled*/
+ const devMode = process.env.NODE_ENV !== 'production'
+ let $SOURCEMAP = devMode ? true : false
+  
+module.exports = {
+  sourceMap: $SOURCEMAP,
+  // importLoaders: 2,
+  ident: 'postcss',
+  plugins: [
+    require('postcss-import')(),
+    require('postcss-flexbugs-fixes')(),
+    require('postcss-preset-env')({
+      stage: 3,
+      autoprefixer: { flexbox: 'no-2009', grid: true, },
+      features: {
+      'nesting-rules': true,
+      'color-mod-function': { unresolved: 'warn', }
+      }
+    }),
+    require('cssnano')( {"preset": ["advanced", { "discardComments": {"removeAll": true,} }], } ),
+  ],  
+}
